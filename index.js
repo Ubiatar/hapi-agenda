e1ddf149d11f1ad2c564a016d34c45fb6ab2fc6f
ubiatar_dev <antonio.ubiatar@gmail.com>
2018-02-05 11:12:19 +0100

    Fix for latest Agenda version

--- index.js
+++ index.js
@@ -3,101 +3,106 @@ var Batch = require('agenda-batch');
 var _ = require('lodash-node');
 var fs = require('fs');
 
-exports.register = function(plugin, options, next) {
+exports.register = function (plugin, options, next) {
   var mongoUrl = options.mongoUrl || 'localhost:27017/hapi-agenda';
-
   var agenda = new Agenda({
-    db: { address: mongoUrl },
+    db: {address: mongoUrl, collection: options.collection},
     processEvery: options.processEvery || '5 minutes'
-  });
+  }, function (err) {
+    if (err) {
+      console.log(err)
+      throw err
+    }
+    agenda.emit('ready')
+    agenda.start();
 
-  // Define the agenda handlers
-  agenda.on('start', function(job) {
-    plugin.log(['agenda', 'start'], job.attrs);
-  });
+    // Define the agenda handlers
+    agenda.on('start', function (job) {
+      plugin.log(['agenda', 'start'], job.attrs);
+    });
 
-  agenda.on('complete', function(job) {
-    plugin.log(['agenda', 'complete'], job.attrs);
-  });
+    agenda.on('complete', function (job) {
+      plugin.log(['agenda', 'complete'], job.attrs);
+    });
 
-  agenda.on('success', function(job) {
-    plugin.log(['agenda', 'success'], job.attrs);
-  });
+    agenda.on('success', function (job) {
+      plugin.log(['agenda', 'success'], job.attrs);
+    });
 
-  agenda.on('fail', function(err, job) {
-    plugin.log(['agenda', 'error'], { err: err, job: job.attrs });
-  });
+    agenda.on('fail', function (err, job) {
+      plugin.log(['agenda', 'error'], {err: err, job: job.attrs});
+    });
 
-  var jobs = {};
-  if (options.jobs && fs.existsSync(options.jobs)) {
-    jobs = require('require-all')(options.jobs);
-  }
-
-  _.forIn(jobs, function(value, key) {
-    var opts = {};
-    var name, method;
-
-    if (typeof value === 'function') {
-      name = key;
-      method = value;
-    } else {
-      name = value.name;
-      method = value.job;
-
-      delete value.name;
-      delete value.job;
-
-      opts = value;
+    var jobs = {};
+    if (options.jobs && fs.existsSync(options.jobs)) {
+      jobs = require('require-all')(options.jobs);
     }
 
-    agenda.define(name, opts, function(job, done) {
-      plugin.log(['agenda', 'queue'], { jobName: name, job: job.attrs });
-      method.call(plugin, job, done);
-    });
+    _.forIn(jobs, function (value, key) {
+      var opts = {};
+      var name, method;
 
-  });
+      if (typeof value === 'function') {
+        name = key;
+        method = value;
+      } else {
+        name = value.name;
+        method = value.job;
 
-  if (options.every) {
-    _.forIn(options.every, function(opts, jobName) {
-      var interval = (typeof opts === 'string') ? opts : opts.interval;
-      var enabled = (opts.enabled !== undefined)? opts.enabled : true;
+        delete value.name;
+        delete value.job;
 
-      if (enabled == false) {
-        return agenda.cancel({name: jobName}, function(err, numRemoved) {
-          if(err)
-          {
-            throw err;
-          }
-        });
+        opts = value;
       }
 
-      agenda.every(interval, jobName);
+      agenda.define(name, opts, function (job, done) {
+        plugin.log(['agenda', 'queue'], {jobName: name, job: job.attrs});
+        method.call(plugin, job, done);
+      });
+
     });
-  }
 
-  plugin.on('start', function() {
-    agenda.start();
-  });
+    if (options.every) {
+      _.forIn(options.every, function (opts, jobName) {
+        var interval = (typeof opts === 'string') ? opts : opts.interval;
+        var enabled = (opts.enabled !== undefined) ? opts.enabled : true;
 
-  var basePath = options.basePath || '';
-  var auth = options.auth || false;
+        if (enabled == false) {
+          return agenda.cancel({name: jobName}, function (err, numRemoved) {
+            if (err) {
+              throw err;
+            }
+          });
+        }
 
-  var batch = new Batch(agenda);
+        agenda.every(interval, jobName);
+      });
+    }
 
-  plugin.expose('agenda', agenda);
-  plugin.expose('batch', batch);
+    plugin.on('start', function () {
+      agenda.start();
+    });
 
-  plugin.bind({
-    batch: batch,
-    agenda: agenda,
-    options: options
-  });
+    var basePath = options.basePath || '';
+    var auth = options.auth || false;
 
-  if (options.jsonApi) {
-    plugin.route(_.values(require('./json-handler')(basePath, auth)));
-  }
+    var batch = new Batch(agenda);
 
-  next();
+    plugin.expose('agenda', agenda);
+    plugin.expose('batch', batch);
+
+    plugin.bind({
+      batch: batch,
+      agenda: agenda,
+      options: options
+    });
+
+    if (options.jsonApi) {
+      plugin.route(_.values(require('./json-handler')(basePath, auth)));
+    }
+
+    next();
+  })
 
 };
 
